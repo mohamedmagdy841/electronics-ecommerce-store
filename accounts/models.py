321229bcd299fc527a61422c9ee186e404df9780
magdy <mmagdy841@gmail.com>
2025-07-30 16:19:25 +0300

    created user and user_profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email and not extra_fields.get('phone_number'):
+            raise ValueError('Users must provide either an email or phone number')
+        
+        if email:
+            email = self.normalize_email(email)
+            
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('role', User.ADMIN)
+        
+        if extra_fields.get('role') != User.ADMIN:
+            raise ValueError('Superuser must have role of Admin.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ADMIN = 1
+    CUSTOMER = 2
+    
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (CUSTOMER, 'Customer'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=50, unique=True, null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email or self.phone_number or self.username
+    
+    class Meta:
+        ordering = ['-created_date']
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='users/profile_pictures', blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    postal_code = models.CharField(max_length=6, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.email
