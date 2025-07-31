00086890e7ae2d252ae0b1a5e8e08f07536c5fdf
magdy <mmagdy841@gmail.com>
2025-07-31 19:26:19 +0300

    changed time zone

--- backend/api_urls.py
+++ backend/api_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from accounts.views import CustomLoginView, CustomRegisterView, SendOtpView, VerifyOtpView
 
@@ -10,5 +10,5 @@ urlpatterns = [
     path('auth/users/verify-otp/', VerifyOtpView.as_view(), name='verify-otp'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    re_path(r'^auth/', include('djoser.social.urls')),
+    path('auth/', include('djoser.social.urls'))
 ]
