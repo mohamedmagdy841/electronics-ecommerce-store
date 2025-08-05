8c7f13deefe4c9677313b1ab9909267e340e473e
magdy <mmagdy841@gmail.com>
2025-08-05 11:51:58 +0300

    added variants products and variant specs models

--- products/models.py
+++ products/models.py
@@ -57,9 +57,6 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    sku = models.CharField(max_length=100, unique=True, db_index=True)
     warranty_years = models.PositiveSmallIntegerField(blank=True, null=True)
     condition = models.CharField(
         max_length=20,
@@ -101,10 +98,36 @@ class ProductSpecification(models.Model):
     
     def __str__(self):
         return f"{self.product}: {self.specification.name} = {self.value}"
+    
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variants')
+    sku = models.CharField(max_length=100, unique=True, db_index=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.sku}"
+
+class VariantSpecification(models.Model):
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, related_name='specs')
+    specification = models.ForeignKey(Specification, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('variant', 'specification')
+
+    def __str__(self):
+        return f"{self.variant}: {self.specification.name} = {self.value}"
 
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, related_name='images', null=True, blank=True)
     url = models.ImageField(upload_to="products/")
     alt_text = models.CharField(max_length=255, blank=True, null=True, help_text="Alternative text for accessibility/SEO.")
     caption = models.CharField(max_length=255, blank=True, null=True, help_text="Short caption or description of the image.")
